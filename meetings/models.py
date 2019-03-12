f3541ce0d0ba056c1ba1b9dcb27fee0f538d60b5
Sunip88 <drzep4@gmail.com>
2019-03-12 14:46:45 +0100

    Meeting model

--- meetings/models.py
+++ meetings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    localization = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    guests = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.title
