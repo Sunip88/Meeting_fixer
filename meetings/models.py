74970fd83f7d5236373e01fa863625c61dfa5864
Sunip88 <drzep4@gmail.com>
2019-03-12 15:09:24 +0100

    starting point

--- meetings/models.py
+++ meetings/models.py
@@ -8,8 +8,8 @@ class Meeting(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     localization = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    guests = models.ManyToManyField(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    guests = models.ManyToManyField(User, related_name='guests')
 
     def __str__(self):
         return self.title
