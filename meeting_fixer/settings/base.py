cde7678301c4091f4117df07f77719ec08d1e933
Sunip88 <drzep4@gmail.com>
2019-03-19 16:35:16 +0100

    settings changes

--- meeting_fixer/settings/base.py
+++ meeting_fixer/settings/base.py
@@ -24,11 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
