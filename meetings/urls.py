18095c6dfb390205f576c8193a4e767021618caf
Sunip88 <drzep4@gmail.com>
2019-03-26 19:27:59 +0100

    start meeting view

--- meetings/urls.py
+++ meetings/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path, include
 
-from views import Home
+from .views import Home, StartMeetingView
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
+    path("start-meeting/", StartMeetingView.as_view(), name="start-meeting"),
 ]
--- meetings/views.py
+++ meetings/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import CreateView
+
+from .models import Meeting
 
 
 class Home(View):
     def get(self, request):
         return render(request, 'meetings/main.html')
+
+
+class StartMeetingView(CreateView):
+    model = Meeting
+    template_name = 'meetings/start_meeting.html'
+    fields = '__all__'
